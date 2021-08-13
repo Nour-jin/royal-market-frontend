91e39accea6ca10a6ac3b2aa3c2d40a125391961
Nour Eddin <69240670+Nour-jin@users.noreply.github.com>
2021-08-13 15:14:27 +0200

    Update service-info.js

--- src/components/api/service-info.js
+++ src/components/api/service-info.js
@@ -1,3 +1,3 @@
-//export const API_ROOT = 'https://online-shop-by-jin.herokuapp.com/products';
- export const img_url = "http://localhost:3001/"
- export const API_ROOT = 'http://localhost:3001/products';
+export const API_ROOT = 'https://online-shop-by-jin.herokuapp.com/products';
+ export const img_url = "https://online-shop-by-jin.herokuapp.com/"
+// export const API_ROOT = 'http://localhost:3001/products';
