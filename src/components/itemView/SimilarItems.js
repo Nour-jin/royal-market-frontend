3a9cb9a2d72df3a9ddec20d5668f9fa005914253
Nour Eddin <69240670+Nour-jin@users.noreply.github.com>
2021-08-14 18:58:40 +0200

    fix simmler component and change view

--- src/components/itemView/SimilarItems.js
+++ src/components/itemView/SimilarItems.js
@@ -2,11 +2,21 @@ import { React, useContext } from "react";
 import SimlerScreen from "./SimlerScreen";
 import { prodactsConext } from "../context/contextData";
 import {SimilaerTitleSvg} from "../watchList/TitleSvg";
-import Masonry, { ResponsiveMasonry } from "react-responsive-masonry"
+import { Swiper, SwiperSlide } from "swiper/react";
+import SwiperCore, {
+  Pagination
+} from 'swiper/core';
+import "swiper/swiper.min.css";
+import "swiper/components/pagination/pagination.min.css"
 const SimilarItems = () => {
   const { recent } = useContext(prodactsConext);
 
-
+  const pagination = {
+    "clickable": true,
+    "renderBullet": function (index, className) {
+            return '<span class=\"' + className + '\">' + (index + 1) + '</span>';
+          }
+  }
 
 
   return (
@@ -14,13 +24,12 @@ const SimilarItems = () => {
       {recent.length ? <SimilaerTitleSvg/> : ""}
      { recent.length ?<div className="sections row mt-3">
         <div className="row cardRow p-5">
-        <ResponsiveMasonry
-            columnsCountBreakPoints={{ 350: 1, 750: 2, 900: 3, 1200:4 }}
-            >
-            <Masonry gutter={50}>
-                      {recent.map((el) => <SimlerScreen key={el._id} el={el} />)}
-                      </Masonry>
-            </ResponsiveMasonry>
+        
+        <Swiper      slidesPerView={4}
+      spaceBetween={50} pagination={pagination}  className="mySwiper">
+              {recent.map((el) => <SwiperSlide><SimlerScreen el={el} /></SwiperSlide>)}
+              </Swiper>
+
         </div>
       </div> :""}
     </div>
